59bbdf033636fa64230455613c6653271c0b7e73
kirankabbur2272006 <kirankabbur22072006@gmail.com>
2025-11-29 13:00:41 +0530

    max

---
+++ max2.py
@@ -0,0 +1,15 @@
+import sys
+
+if len(sys.argv) > 1:
+    scores = [int(x) for x in sys.argv[1:]]
+    print("User provided scores:", scores)
+else:
+    print("No input provided – using default values")
+    scores = [50, 60, 70, 80, 90]
+
+
+total = sum(scores)
+average = total / len(scores)
+
+print("Sum of scores:", total)
+print("Average of scores:", average)
